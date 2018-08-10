d90d60fadc1cfc470591df8f29e1381d8d8d8ca5
srdesai <isriramdesai@gmail.com>
2018-08-10 15:15:26 +0530

    flush, royalflush and evaluate.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
     public static void main(String [] args) {
-
+        Card cards[] = new Card[5];
+        cards[0] = new Card('A', 'D');
+        cards[1] = new Card('K', 'D');
+        cards[2] = new Card('Q', 'S');
+        cards[3] = new Card('J', 'D');
+        cards[4] = new Card('1', 'D');
+        Hand hand = new Hand(cards);
+        System.out.println(hand.hasRoyalFlush());
     }
 
 }
