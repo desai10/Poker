0262cd8606895ad231ebccbc30fe83be9d01ee9f
srdesai <isriramdesai@gmail.com>
2018-08-10 14:25:08 +0530

    init

--- src/Hand.java
+++ src/Hand.java
@@ -1,2 +1,43 @@
+import java.util.List;
+
 public class Hand {
+
+    Card cards[] = new Card[5];
+
+    public Hand(Card [] cards) {
+        this.cards = cards;
+    }
+
+    public int evaluate() {
+        return 0;
+    }
+
+    private boolean hasPair() {
+        return false;
+    }
+
+    private boolean hasNOfAKind(int n) {
+        return false;
+    }
+
+    private boolean hasFlush() {
+        return false;
+    }
+
+    private boolean hasStraight() {
+        return false;
+    }
+
+    private boolean hasFullHouse() {
+        return hasPair() && hasNOfAKind(3);
+    }
+
+    private boolean hasRoyalFlush() {
+        return false;
+    }
+
+    private boolean hasStraightFlush() {
+        return hasStraight() && hasFlush();
+    }
+
 }
