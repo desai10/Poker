8f90f44cf231942028f9ee72f10cde37ac92b770
srdesai <isriramdesai@gmail.com>
2018-08-10 14:18:32 +0530

    init

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,19 @@
 public class Card {
+
+    char faceValue;
+    char suit;
+
+    public Card(char faceValue, char suit) {
+        this.faceValue = faceValue;
+        this.suit = suit;
+    }
+
+    public char getFaceValue() {
+        return faceValue;
+    }
+
+    public char getSuit() {
+        return suit;
+    }
+
 }
